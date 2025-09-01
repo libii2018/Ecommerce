21b7ef5aaef43471fa841bcf21cb10eed3e7283e
LIBII <libiijoseph@gmail.com>
2025-09-01 09:20:28 +0100

    Relationships with Cart

--- src/main/java/com/ecommerce/project/model/User.java
+++ src/main/java/com/ecommerce/project/model/User.java
@@ -64,6 +64,10 @@ public class User {
     private List<Address> addresses = new ArrayList<>();
 
     @ToString.Exclude
+    @OneToOne(mappedBy = "user", cascade = {CascadeType.PERSIST, CascadeType.MERGE}, orphanRemoval = true)
+    private Cart cart;
+
+
     @OneToMany(mappedBy = "user",
             cascade = {CascadeType.PERSIST, CascadeType.MERGE},
             orphanRemoval = true)
